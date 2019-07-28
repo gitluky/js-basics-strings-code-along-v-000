749622ac708c849d48335942d27c49deebfaa589
Jonathan Luk <jluk87@gmail.com>
2019-07-28 22:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, currentUser.indexOf(1)!`
